75aac19d77db46eb7f2777c9716ba8fae9065074
Trishula Software <engineering@trishulasoftware.com>
2026-04-27 16:28:55 -0400

    fix(ci): Root-cause remediation — add setup.py for Cython build, timeout for daemon scripts, mkdir telemetry

---
+++ setup.py
@@ -0,0 +1,8 @@
+﻿from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name="DeterministicSQA",
+    ext_modules=cythonize("mcdc_tester.py", compiler_directives={'language_level': "3"}),
+    zip_safe=False,
+)
